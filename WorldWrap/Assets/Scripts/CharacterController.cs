2bb0b4bf8a096eeb0279bbd5e707ebafeaa14f83
MLivanos <prodigiousmike@gmail.com>
2023-06-13 18:26:14 -0700

    ADD: First-to-third Person View Switch
    ADD: Player can now switch between first and third person views using the "V" key at any point.

--- WorldWrap/Assets/Scripts/CharacterController.cs
+++ WorldWrap/Assets/Scripts/CharacterController.cs
@@ -6,18 +6,51 @@ public class CharacterController : MonoBehaviour
 {
     [SerializeField] private float rotationSensitivity;
     [SerializeField] private float speed;
+    private Camera mainCamera;
+    private GameObject mainCameraGameObject;
+    private bool isFirstPerson;
 
-    void Start()
+    private void Start()
     {
-        
+        mainCamera = gameObject.GetComponentInChildren<Camera>();
+        mainCameraGameObject = mainCamera.gameObject;
+        isFirstPerson = true;
     }
 
-    void Update()
+    private void Update()
+    {
+        if (Input.GetKeyDown("v"))
+        {
+            ChangeCameraView();
+        }
+        UpdatePosition();
+    }
+
+    private void UpdatePosition()
     {
         float rotationSpeed = Input.GetAxisRaw("Horizontal") * Time.deltaTime * rotationSensitivity;
         float movementSpeed = Input.GetAxisRaw("Vertical") * Time.deltaTime * speed;
         
         transform.Rotate(0, rotationSpeed, 0); 
         transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
+    }
+
+    private void ChangeCameraView()
+    {
+        if (isFirstPerson)
+        {
+            // Establish third-person view
+            mainCameraGameObject.transform.parent = null;
+            mainCameraGameObject.transform.position = new Vector3(0, 80, 0);
+            mainCameraGameObject.transform.eulerAngles = new Vector3(90, 0, 0);
+        }
+        else
+        {
+            // Establish first-person view
+            mainCameraGameObject.transform.parent = gameObject.transform;
+            mainCameraGameObject.transform.localPosition = new Vector3(0, 0.25f, 0.5f);
+            mainCameraGameObject.transform.rotation = gameObject.transform.rotation;
+        }
+        isFirstPerson = !isFirstPerson;
     }
 }
