483f81bf04715ee8f378cb368f0452cef6f4dc01
MLivanos <prodigiousmike@gmail.com>
2023-06-14 17:22:33 -0700

    ADD: WrapManager automatically detects and documents map shape
    ADD: The WrapManager class, given the blocks used to create the wrapping effect, automatically mines the map's shape and creates a block matrix data structure to facilitate wrapping.

--- WorldWrap/Assets/Scripts/WarpManager.cs
+++ WorldWrap/Assets/Scripts/WarpManager.cs
@@ -7,15 +7,22 @@ using UnityEngine;
 public class WarpManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] blocks;
-    private GameObject[][] blockMatrix;
+    private GameObject[,] blockMatrix;
 
     private void Start()
     {
-        OrganizeBlockMatrix();
+        // Automatically detect matrix structure of blocks
+        Vector2[] coordinatesByX;
+        Vector2[] coordinatesByZ;
+        Dictionary<float, int> xToRow = new Dictionary<float, int>();
+        Dictionary<float, int> zToColumn = new Dictionary<float, int>();
+        SortCoordinates(out coordinatesByX, out coordinatesByZ);
+        SetupMatrix(coordinatesByX, coordinatesByZ, xToRow, zToColumn);
+        FillMatrix(xToRow, zToColumn);
     }
 
     // Given the unorganized array of blocks, organize them into a matrix
-    private void OrganizeBlockMatrix()
+    private void SortCoordinates(out Vector2[] coordinatesByX, out Vector2[] coordinatesByZ)
     {
         Vector2[] coordinates = new Vector2[blocks.Length];
         for(int blockIndex = 0; blockIndex < blocks.Length; blockIndex += 1)
@@ -25,29 +32,48 @@ public class WarpManager : MonoBehaviour
             coordinates[blockIndex] = new Vector2(blockX, blockZ);
         }
         List<Vector2> coordinatesList = new List<Vector2>(coordinates);
-        Vector2[] coordinatesByX = coordinatesList.OrderBy(v => v.x).ToArray();
-        Vector2[] coordinatesByZ = coordinatesList.OrderBy(v => v.y).ToArray();
+        coordinatesByX = coordinatesList.OrderBy(v => v.x).ToArray();
+        coordinatesByZ = coordinatesList.OrderBy(v => v.y).ToArray();
+    }
+
+    // Detect matrix shape and instantiate it
+    private void SetupMatrix(Vector2[] coordinatesByX, Vector2[] coordinatesByZ, Dictionary<float, int> xToRow, Dictionary<float, int> zToColumn)
+    {
         int numberOfRows = 1;
         int numberOfColumns = 1;
         float previousX = coordinatesByX[0].x;
         float previousZ = coordinatesByZ[0].y;
+        xToRow[previousX] = 0;
+        zToColumn[previousX] = 0;
         for(int blockIndex = 0; blockIndex < blocks.Length; blockIndex += 1)
         {
             float x = coordinatesByX[blockIndex].x;
             float z = coordinatesByZ[blockIndex].y;
             if(x != previousX)
             {
+                xToRow[x] = numberOfRows;
                 numberOfRows += 1;
                 previousX = x;
             }
             if(z != previousZ)
             {
+                xToRow[z] = numberOfColumns;
                 numberOfColumns += 1;
                 previousZ = z;
             }
         }
-        Debug.Log(numberOfRows);
-        Debug.Log(numberOfColumns);
+        blockMatrix = new GameObject[numberOfRows,numberOfColumns];
+    }
+
+    // Add blocks to their position according to where they exist in the world.
+    private void FillMatrix(Dictionary<float, int> xToRow, Dictionary<float, int> zToColumn)
+    {
+        foreach(GameObject block in blocks)
+        {
+            int row = xToRow[block.transform.position.x];
+            int column = xToRow[block.transform.position.z];
+            blockMatrix[row, column] = block;
+        }
     }
 
     public void LogEntry(string triggerName)
