a3d47f0be87a8afab99c1ce3c108d5f6a9d314de
MLivanos <prodigiousmike@gmail.com>
2023-06-15 16:45:58 -0700

    ADD: Adds Basic Interaction Capabilities With Grabbable Objects
    ADD: Adds a basic interaction system in which any object with the "grabbable" tag can be grabbed by the player with the e key and subsequently thrown with the same key.

--- WorldWrap/Assets/Scripts/CharacterController.cs
+++ WorldWrap/Assets/Scripts/CharacterController.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    // Movement variables
     [SerializeField] private float rotationSensitivity;
     [SerializeField] private float speed;
+    // Camera variables
     private Camera mainCamera;
     private GameObject mainCameraGameObject;
     private Vector3 mainCameraFPPosition;
     private bool isFirstPerson;
+    // Interaction variables
+    [SerializeField] private Vector3 heldObjectPosition;
+    [SerializeField] private float grabbingRange;
+    [SerializeField] private float throwStrength;
+    private GameObject heldObject;
+    private bool isHoldingObject;
 
     private void Start()
     {
-        mainCamera = gameObject.GetComponentInChildren<Camera>();
-        mainCameraGameObject = mainCamera.gameObject;
-        mainCameraFPPosition = mainCameraGameObject.transform.localPosition;
-        isFirstPerson = true;
+        SetupCamera();
+        isHoldingObject = false;
     }
 
     private void Update()
@@ -25,7 +31,19 @@ public class CharacterController : MonoBehaviour
         {
             ChangeCameraView();
         }
+        if (Input.GetKeyDown("e"))
+        {
+            Interact();
+        }
         UpdatePosition();
+    }
+
+    private void SetupCamera()
+    {
+        mainCamera = gameObject.GetComponentInChildren<Camera>();
+        mainCameraGameObject = mainCamera.gameObject;
+        mainCameraFPPosition = mainCameraGameObject.transform.localPosition;
+        isFirstPerson = true;
     }
 
     private void UpdatePosition()
@@ -35,6 +53,42 @@ public class CharacterController : MonoBehaviour
         
         transform.Rotate(0, rotationSpeed, 0); 
         transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
+    }
+
+    private void Interact()
+    {
+        if (isHoldingObject)
+        {
+            ThrowObject();
+        }
+        else
+        {
+            PickUpObject();
+        }
+        
+    }
+
+    private void PickUpObject()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, grabbingRange))
+        {
+            if (hit.rigidbody != null && hit.rigidbody.tag == "Grabbable")
+            {
+                hit.rigidbody.gameObject.transform.parent = gameObject.transform;
+                hit.rigidbody.gameObject.transform.localPosition = heldObjectPosition;
+                heldObject = hit.rigidbody.gameObject;
+                isHoldingObject = true;
+            }
+        }
+    }
+
+    private void ThrowObject()
+    {
+        heldObject.transform.parent = null;
+        Rigidbody objectRigidBody = heldObject.GetComponent<Rigidbody>();
+        objectRigidBody.AddForce(throwStrength * transform.TransformDirection(Vector3.forward), ForceMode.Impulse);
+        isHoldingObject = false;
     }
 
     private void ChangeCameraView()
