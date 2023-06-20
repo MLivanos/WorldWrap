0e14c9f87f005d93df5057008b577360771fb2fe
MLivanos <prodigiousmike@gmail.com>
2023-06-20 02:24:52 -0700

    ADD: Adds dodgeballs that can be thrown at actors to reduce health or kill them
    ADD: Adds dodgeballs that can be thrown at actors to reduce health or kill them. Includes Dodgeball class, DodgeballActor, DodgeballPlayer, DodgeballEnemy.

---
+++ WorldWrap/Assets/Scripts/Dodgeball/DodgeballEnemy.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodgeballEnemy : DodgeballActor
+{
+    private void Start()
+    {
+        health = 2;
+    }
+}
