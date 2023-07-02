1bb622d74aee881e57fb2d082f7ab8ce1ce1b19c
MLivanos <prodigiousmike@gmail.com>
2023-07-02 10:28:17 -0700

    ADD: Allows trees to be generated at runtime in scale test
    ADD: Allows trees to be generated at runtime in scale test

--- WorldWrap/Assets/Tests/Scripts/ScaleTests/InputManager.cs
+++ WorldWrap/Assets/Tests/Scripts/ScaleTests/InputManager.cs
@@ -9,21 +9,23 @@ public class InputManager : MonoBehaviour
     [SerializeField] private GameObject globalBounds;
     [SerializeField] private float ballQuantity;
     [SerializeField] private float treeQuantity;
-    private float xMin;
-    private float xMax;
-    private float zMin;
-    private float zMax;
+    private float globalXMin;
+    private float globalXMax;
+    private float globalZMin;
+    private float globalZMax;
 
 
     private void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         BoundsTrigger boundsTrigger = globalBounds.GetComponent<BoundsTrigger>();
         Vector2 xBounds = boundsTrigger.getXBounds();
         Vector2 zBounds = boundsTrigger.getZBounds();
-        xMin = xBounds[0];
-        xMax = xBounds[1];
-        zMin = zBounds[0];
-        zMax = zBounds[1];
+        globalXMin = xBounds[0];
+        globalXMax = xBounds[1];
+        globalZMin = zBounds[0];
+        globalZMax = zBounds[1];
     }
 
     private void Update()
@@ -42,18 +44,41 @@ public class InputManager : MonoBehaviour
     {
         for(int ballNumber = 0; ballNumber < ballQuantity; ballNumber++)
         {
-            Instantiate(ballPrefab, GetRandomPosition(), Quaternion.identity);
+            InstantiateNewBall();
         }
     }
 
     private void AddTree()
     {
-        
+        foreach(Transform blockTransform in globalBounds.transform)
+        {
+            for(int treeNumber = 0; treeNumber < treeQuantity; treeNumber++)
+            {
+                GameObject centerCube = blockTransform.Find("Cube").gameObject;
+                ParentNewTree(centerCube);
+            }
+        }
     }
 
-    private Vector3 GetRandomPosition()
+    private Vector3 GetRandomPosition(float xMin, float xMax, float zMin, float zMax, float yPosition)
     {
-        return new Vector3(Random.Range(xMin, xMax), 2.0f, Random.Range(zMin, zMax));
+        return new Vector3(Random.Range(xMin, xMax), yPosition, Random.Range(zMin, zMax));
+    }
+
+    private void InstantiateNewBall()
+    {
+        GameObject newBall = Instantiate(ballPrefab, GetRandomPosition(globalXMin, globalXMax, globalZMin, globalZMax, 2.0f), Quaternion.identity);
+        newBall.GetComponent<Renderer>().material.color = Random.ColorHSV();
+    }
+
+    private void ParentNewTree(GameObject centerCube)
+    {
+        GameObject newTree = Instantiate(treePrefab, Vector3.zero, Quaternion.identity);
+        newTree.transform.parent = centerCube.transform;
+        newTree.GetComponent<Renderer>().material.color = Random.ColorHSV();
+        float cubeSize = 0.5f;
+        Vector3 newPos = GetRandomPosition(-cubeSize, cubeSize, -cubeSize, cubeSize, 0.75f);
+        newTree.transform.localPosition = newPos;
     }
 
 }
