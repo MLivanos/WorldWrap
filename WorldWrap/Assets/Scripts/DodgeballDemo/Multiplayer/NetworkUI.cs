727c096d2ffbb0e272b23acdc57ae495f2fa9b90
MLivanos <prodigiousmike@gmail.com>
2023-07-11 17:03:36 -0700

    💄UI: Adds server, host, and client buttons
    💄UI: Adds server, host, and client buttons for easy connecting

---
+++ WorldWrap/Assets/Scripts/DodgeballDemo/Multiplayer/NetworkUI.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkUI : MonoBehaviour
+{
+    [SerializeField] private Button serverButton;
+    [SerializeField] private Button hostButton;
+    [SerializeField] private Button clientButton;
+
+    private void Awake()
+    {
+        serverButton.onClick.AddListener(()=>
+        {
+            NetworkManager.Singleton.StartServer();
+        });
+
+        hostButton.onClick.AddListener(()=>
+        {
+            NetworkManager.Singleton.StartHost();
+        });
+
+        clientButton.onClick.AddListener(()=>
+        {
+            NetworkManager.Singleton.StartClient();
+        });
+    }
+}
